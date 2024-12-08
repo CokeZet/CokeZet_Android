12990e1090521e9f741b20df8c77e4039ec4f852
주재완 <yjjw2000@naver.com>
2024-12-08 19:59:30 +0900

    design : SplashScreen 구현

---
+++ presentation/src/main/java/buy/coke/zet/presentation/intro/splash/SplashActivity.kt
@@ -0,0 +1,19 @@
+package buy.coke.zet.presentation.intro.splash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import buy.coke.zet.presentation.R
+import buy.coke.zet.presentation.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+    }
+}
