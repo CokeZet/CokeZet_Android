425eefc2cb60422b5f838b3d297fe622780a356b
주재완 <yjjw2000@naver.com>
2025-01-07 19:44:58 +0900

    design : Product Detail 디자인 80% 완료

---
+++ presentation/src/main/java/buy/coke/zet/presentation/product/detail/ProductDetailActivity.kt
@@ -0,0 +1,16 @@
+package buy.coke.zet.presentation.product.detail
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import buy.coke.zet.presentation.R
+
+class ProductDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+
+    }
+}
