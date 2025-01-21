6f7ef1b9fde9071981aca557c637a58648b4b634
주재완 <yjjw2000@naver.com>
2025-01-21 20:23:51 +0900

    design : Product Detail 디자인 구현

--- presentation/src/main/java/buy/coke/zet/presentation/product/detail/ProductDetailActivity.kt
+++ presentation/src/main/java/buy/coke/zet/presentation/product/detail/ProductDetailActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import buy.coke.zet.presentation.R
+import buy.coke.zet.presentation.databinding.ActivityProductDetailBinding
+import buy.coke.zet.presentation.databinding.ActivityProductDetailBindingImpl
 
 class ProductDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_detail)
     }
 }
