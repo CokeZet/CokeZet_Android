6685c04bd3a0fd173c1d1d57da93f1cb716e14e4
주재완 <yjjw2000@naver.com>
2025-02-02 19:20:27 +0900

    feature : 알림 권한 설정

--- presentation/src/main/java/buy/coke/zet/presentation/intro/entry/EntryActivity.kt
+++ presentation/src/main/java/buy/coke/zet/presentation/intro/entry/EntryActivity.kt
@@ -1,10 +1,17 @@
 package buy.coke.zet.presentation.intro.entry
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
@@ -15,14 +22,32 @@ import setting.NicknameSettingActivity
 
 class EntryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEntryBinding
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_entry)
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                Toast.makeText(this, "알림 권한이 허용되었습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "알림 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+            startActivity(Intent(this, NicknameSettingActivity::class.java))
+        }
 
         binding.startButton.clickListener = View.OnClickListener {
-            ShortDialog(this).show()
-            startActivity(Intent(this, NicknameSettingActivity::class.java))
+            ShortDialog(
+                context = this,
+                yesButtonListener = {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                        ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    }
+                    else {
+                        startActivity(Intent(this, NicknameSettingActivity::class.java))
+                    }
+                }).show()
         }
     }
 }
