8db1142d8e4a9e1f71a41e3aac751c6c4feb4e14
주재완 <yjjw2000@naver.com>
2025-03-04 19:16:04 +0900

    design : 마이페이지 피드백 반영

--- presentation/src/main/java/buy/coke/zet/presentation/info/mypage/MyPageActivity.kt
+++ presentation/src/main/java/buy/coke/zet/presentation/info/mypage/MyPageActivity.kt
@@ -2,6 +2,9 @@ package buy.coke.zet.presentation.info.mypage
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -9,6 +12,8 @@ import buy.coke.zet.presentation.R
 import buy.coke.zet.presentation.databinding.ActivityMyPageBinding
 import buy.coke.zet.presentation.info.announcement.AnnouncementActivity
 import buy.coke.zet.presentation.info.notification_setting.NotificationSettingActivity
+import buy.coke.zet.presentation.info.term.TERM_TITLE
+import buy.coke.zet.presentation.info.term.TermActivity
 
 class MyPageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyPageBinding
@@ -16,11 +21,30 @@ class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_my_page)
+        val originalText = binding.userNickname.text.toString()
+        binding.userNickname.text = SpannableString(originalText).apply {
+            val start = originalText.indexOf("님")
+            val end = start + 1
+
+            this.setSpan(ForegroundColorSpan(getColor(buy.coke.zet.common.R.color.gray_500)), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
 
         binding.setPriceNotificationButton.root.setOnClickListener { startActivity(Intent(this, NotificationSettingActivity::class.java)) }
         binding.announcementButton.root.setOnClickListener { startActivity(Intent(this, AnnouncementActivity::class.java)) }
-        binding.mypageTopbar.leftIconClickListener = View.OnClickListener {
-            finish()
+        binding.serviceTermButton.root.setOnClickListener {
+            val intent = Intent(this, TermActivity::class.java).apply {
+                putExtra(TERM_TITLE, getString(R.string.service_term))
+            }
+
+            startActivity(intent)
         }
+        binding.personalInformationTermButton.root.setOnClickListener {
+            val intent = Intent(this, TermActivity::class.java).apply {
+                putExtra(TERM_TITLE, getString(R.string.service_term))
+            }
+
+            startActivity(intent)
+        }
+        binding.mypageTopbar.leftIconClickListener = View.OnClickListener { finish() }
     }
 }
