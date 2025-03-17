2edb961cb99369ae1deef67326de61bc6b8ee6f5
Juseonghyun <jucoding99@gmail.com>
2025-03-18 02:55:53 +0900

    feat: 상수를 한 곳에서 정의
    issue: #24

--- data/src/main/java/buy/coke/zet/data/util/Constants.kt
+++ data/src/main/java/buy/coke/zet/data/util/Constants.kt
@@ -2,6 +2,6 @@ package buy.coke.zet.data.util
 
 object Constants {
     const val USER_PREFERENCES_FILE = "user_preferences_file"
+
     const val USER_ACCESS_TOKEN = "user_access_token"
-    const val USER_REFRESH_TOKEN = "user_refresh_token"
 }
