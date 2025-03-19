039ebc6023b28753bacb245c737cc144cb6dbe71
Juseonghyun <jucoding99@gmail.com>
2025-03-19 20:26:20 +0900

    refactor: logCat 제거및 if -> let 으로 null-check
    issue: #24

--- data/src/main/java/buy/coke/zet/data/errorhandle/SafeApiCall.kt
+++ data/src/main/java/buy/coke/zet/data/errorhandle/SafeApiCall.kt
@@ -1,6 +1,5 @@
 package buy.coke.zet.data.errorhandle
 
-import android.util.Log
 import buy.coke.zet.data.dto.CommonResponseDto
 import buy.coke.zet.domain.ServiceResult
 import retrofit2.Response
@@ -10,18 +9,13 @@ suspend fun <T> safeApiCall(apiCall: suspend () -> Response<CommonResponseDto<T>
     return try {
         val response = apiCall()
         if (response.isSuccessful) {
-            val body = response.body()
-            Log.d("safeApiCall", body?.message.toString())
-            Log.d("safeApiCall", body?.code.toString())
-            if (body != null) {
-                if (body.code == "SUCCESS" && body.data != null) {
+            response.body()?.let { body ->
+                return if (body.code == "SUCCESS" && body.data != null) {
                     ServiceResult.Success(body.data)
                 } else {
                     ServiceResult.Error(body.code ?: "UNKNOWN_ERROR", body.message)
                 }
-            } else {
-                ServiceResult.Error("NULL_BODY", "Response Body의 값이 null 입니다")
-            }
+            } ?: ServiceResult.Error("NULL_BODY", "Response Body의 값이 null 입니다")
         } else {
             ServiceResult.Error(response.code().toString(), response.message())
         }
