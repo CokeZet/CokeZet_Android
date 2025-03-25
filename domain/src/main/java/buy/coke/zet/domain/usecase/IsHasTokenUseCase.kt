64d9801f95a73177a230e306bd5e94ccc2437028
Juseonghyun <jucoding99@gmail.com>
2025-03-26 02:33:31 +0900

    dosc: 각 유스케이스 설명글 추가
    issue: #26

--- domain/src/main/java/buy/coke/zet/domain/usecase/IsHasTokenUseCase.kt
+++ domain/src/main/java/buy/coke/zet/domain/usecase/IsHasTokenUseCase.kt
@@ -3,6 +3,12 @@ package buy.coke.zet.domain.usecase
 import buy.coke.zet.domain.repository.AuthRepository
 import javax.inject.Inject
 
+/** 토큰 유효성 검사 유스케이스
+ * Local DB에 Token이 있는지 없는지를 Boolean 값으로 내려줍니다.
+ * 해당 Boolean 값을 IsValidTokenUseCase에 값을 전달 해주시면 됩니다.
+ * true = Token 있음, false = Token 없음
+ */
+
 class IsHasTokenUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
