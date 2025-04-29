c28dd2490592cce2a7e8d00fc2fe3cc8dcbcd390
Juseonghyun <jucoding99@gmail.com>
2025-04-29 21:32:17 +0900

    feat: GetProducts Api 모듈 설정및 인터페이스, Response값 설정
    issue: #45

--- data/src/main/java/buy/coke/zet/data/di/NetworkModule.kt
+++ data/src/main/java/buy/coke/zet/data/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package buy.coke.zet.data.di
 
 import buy.coke.zet.data.local.TokenManager
 import buy.coke.zet.data.api.AuthApiService
+import buy.coke.zet.data.api.ProductApiService
 import buy.coke.zet.data.api.PromotionApiService
 import buy.coke.zet.data.api.UserApiService
 import buy.coke.zet.data.datasource.AuthDataSource
@@ -80,5 +81,11 @@ object NetworkModule {
     @Singleton
     fun providePromotionApiService(retrofit: Retrofit): PromotionApiService {
         return retrofit.create(PromotionApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductApiService(retrofit: Retrofit): ProductApiService {
+        return retrofit.create(ProductApiService::class.java)
     }
 }
---
+++ data/src/main/java/buy/coke/zet/data/dto/product/GetProductResponse.kt
@@ -0,0 +1,17 @@
+package buy.coke.zet.data.dto.product
+
+import com.google.gson.annotations.SerializedName
+
+data class GetProductResponse(
+    @SerializedName("productId") val productId: Int? = null,
+    @SerializedName("storeProductId") val storeProductId: Int? = null,
+    @SerializedName("price") val price: Int? = null,
+    @SerializedName("pricePerMl") val pricePerMl: Int? = null,
+    @SerializedName("discountRate") val discountRate: Int? = null,
+    @SerializedName("size") val size: String? = null,
+    @SerializedName("brand") val brand: String? = null,
+    @SerializedName("count") val count: Int? = null,
+    @SerializedName("taste") val taste: String? = null,
+    @SerializedName("storeName") val storeName: String? = null,
+    @SerializedName("cardNameList") val cardNameList: String? = null
+)
