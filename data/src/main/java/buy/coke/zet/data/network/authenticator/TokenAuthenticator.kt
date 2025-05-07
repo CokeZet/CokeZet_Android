c9f92395782190f409af05dcb748ce9c48706a86
Juseonghyun <jucoding99@gmail.com>
2025-05-08 00:30:49 +0900

    refactor: TokenAuthenticator의 RefreshToken 요청 동시성 제어를 위해 Mutex 처리
    issue: #50

--- data/src/main/java/buy/coke/zet/data/network/authenticator/TokenAuthenticator.kt
+++ data/src/main/java/buy/coke/zet/data/network/authenticator/TokenAuthenticator.kt
@@ -4,6 +4,8 @@ import buy.coke.zet.data.datasource.AuthDataSource
 import buy.coke.zet.data.local.TokenManager
 import buy.coke.zet.domain.ServiceResult
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import okhttp3.Authenticator
 import okhttp3.Request
 import okhttp3.Response
@@ -16,6 +18,8 @@ class TokenAuthenticator @Inject constructor(
     private val tokenManager: TokenManager
 ) : Authenticator {
 
+    private val mutex = Mutex()
+
     override fun authenticate(route: Route?, response: Response): Request? {
         if (response.code != 401) return null
 
@@ -26,30 +30,32 @@ class TokenAuthenticator @Inject constructor(
         }
 
         return runBlocking {
-            val refreshToken = tokenManager.getRefreshToken()
-            if (refreshToken.isNullOrEmpty()) return@runBlocking null
-            val newTokenResult = try {
-                authDataSource.get().refreshToken(refreshToken)
-            } catch (e: Exception) {
-                tokenManager.clearTokens()
-                return@runBlocking null
-            }
-
-            when (newTokenResult) {
-                is ServiceResult.Success -> {
-                    tokenManager.saveTokens(
-                        newTokenResult.data.accessToken,
-                        newTokenResult.data.refreshToken
-                    )
-
-                    response.request.newBuilder()
-                        .header("Authorization", "Bearer ${newTokenResult.data.accessToken}")
-                        .build()
+            mutex.withLock {
+                val refreshToken = tokenManager.getRefreshToken()
+                if (refreshToken.isNullOrEmpty()) return@runBlocking null
+                val newTokenResult = try {
+                    authDataSource.get().refreshToken(refreshToken)
+                } catch (e: Exception) {
+                    tokenManager.clearTokens()
+                    return@runBlocking null
                 }
 
-                else -> {
-                    tokenManager.clearTokens()
-                    null //TODO :: 로그아웃 처리 하기
+                when (newTokenResult) {
+                    is ServiceResult.Success -> {
+                        tokenManager.saveTokens(
+                            newTokenResult.data.accessToken,
+                            newTokenResult.data.refreshToken
+                        )
+
+                        response.request.newBuilder()
+                            .header("Authorization", "Bearer ${newTokenResult.data.accessToken}")
+                            .build()
+                    }
+
+                    else -> {
+                        tokenManager.clearTokens()
+                        null //TODO :: 로그아웃 처리 하기
+                    }
                 }
             }
         }
