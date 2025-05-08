ad47ceef0d5d40bf18bbc80767e922a3933ac136
Juseonghyun <jucoding99@gmail.com>
2025-05-08 19:17:43 +0900

    feat: Contents 관련 API 모듈 설정및 Dto 추가
    notice, privacy_policy, terms api 추가
    
    issue: #50

---
+++ data/src/main/java/buy/coke/zet/data/dto/contents/terms/GetTermsResponseDto.kt
@@ -0,0 +1,11 @@
+package buy.coke.zet.data.dto.contents.terms
+
+import com.google.gson.annotations.SerializedName
+
+data class GetTermsResponseDto(
+    @SerializedName("id") val id: Long,
+    @SerializedName("type") val type: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("content") val content: String,
+    @SerializedName("updatedAt") val updatedAt: String
+)
