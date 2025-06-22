df1a56c8840f7edb08f6978a4bbc1cad0d040a9a
Juseonghyun <jucoding99@gmail.com>
2025-06-22 21:35:22 +0900

    refactor: 요청값 PATCH로 변경
    issue: #57

--- data/src/main/java/buy/coke/zet/data/api/UserApiService.kt
+++ data/src/main/java/buy/coke/zet/data/api/UserApiService.kt
@@ -3,18 +3,20 @@ package buy.coke.zet.data.api
 import buy.coke.zet.data.dto.CommonResponseDto
 import buy.coke.zet.data.dto.getprofile.GetProfileResponseDto
 import buy.coke.zet.data.dto.updateprofile.UpdateProfileRequestDto
+import buy.coke.zet.data.dto.updateprofile.UpdateProfileResponseDto
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface UserApiService {
     @DELETE("/api/users/profile")
     suspend fun delete(): Response<CommonResponseDto<Unit>>
 
-    @POST("/api/users/profile")
-    suspend fun updateProfile(@Body request: UpdateProfileRequestDto): Response<CommonResponseDto<Unit>>
+    @PATCH("/api/users/profile")
+    suspend fun updateProfile(@Body request: UpdateProfileRequestDto): Response<CommonResponseDto<UpdateProfileResponseDto>>
 
     @GET("/api/users/profile")
     suspend fun getProfile(): Response<CommonResponseDto<GetProfileResponseDto>>
