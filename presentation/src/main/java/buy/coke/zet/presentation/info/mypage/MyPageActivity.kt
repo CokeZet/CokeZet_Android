57c16210c3a9fd52bb332865b8db34f963c5c67e
Juseonghyun <jucoding99@gmail.com>
2025-07-13 01:48:37 +0900

    refactor: Delete 로직 변경 수정
    Delete 로직이 변경됨에 따라 registerForActivityResult로 회원탈퇴 로직을 수정하였습니다.
    
    issue: #59

--- presentation/src/main/java/buy/coke/zet/presentation/info/mypage/MyPageActivity.kt
+++ presentation/src/main/java/buy/coke/zet/presentation/info/mypage/MyPageActivity.kt
@@ -1,8 +1,12 @@
 package buy.coke.zet.presentation.info.mypage
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
@@ -17,6 +21,7 @@ import buy.coke.zet.presentation.info.notification_setting.NotificationSettingAc
 import buy.coke.zet.presentation.info.term.TERM_TITLE
 import buy.coke.zet.presentation.info.term.TermActivity
 import buy.coke.zet.presentation.intro.signup.SignUpActivity
+import buy.coke.zet.presentation.util.GoogleDeleteManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -26,6 +31,24 @@ class MyPageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMyPageBinding
     @Inject lateinit var logoutUseCase: LogoutUseCase
     @Inject lateinit var deleteUseCase: DeleteUseCase
+
+    private val signInLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val authCode = GoogleDeleteManager.extractAuthCode(result.data)
+        if (authCode != null) {
+            lifecycleScope.launch {
+                val accessToken = GoogleDeleteManager.getAccessToken(authCode)
+                if (accessToken != null) {
+                    deleteUseCase(accessToken)
+                } else {
+                    Toast.makeText(this@MyPageActivity, "Google에서 AccessToken 받아오기 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
+        } else {
+            Toast.makeText(this@MyPageActivity, "Google에서 AccessToken 받아오기 실패", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,7 +91,8 @@ class MyPageActivity : AppCompatActivity() {
                 warning = getString(R.string.withdraw_warning),
                 yesButtonListener = {
                     lifecycleScope.launch {
-                        deleteUseCase()
+                        val intent = GoogleDeleteManager.getSignInIntent(Activity())
+                        signInLauncher.launch(intent)
                         finishAffinity()
                     }
                 }
